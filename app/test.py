a934e619cb85c87a2a4c9f43d59d2171a313ab42
Latervia <latervia@qq.com>
2026-05-18 14:10:09 +0800

    优化项目结构

--- app/test.py
+++
@@ -1,8 +0,0 @@
-import posixpath
-
-if __name__ == '__main__':
-    a = "http://100.122.27.37:11434"
-    b= posixpath.join(a, "v1")
-    print(type(b))
-
-    print(b)
--- app/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-# 向后兼容：工具模块已迁移到 app.agent.tools
-from app.agent.tools.registry import ToolRegistry
-from app.agent.tools.message_adapter import MessageAdapter
-
-__all__ = ["ToolRegistry", "MessageAdapter"]
